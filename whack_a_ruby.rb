a8d84d839a47a3a031426b00de2e9b58906f86f9
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-11-09 18:49:43 -0500

    wieder zurück gestellt zu dem Moment wo es nur ein Ruby war

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -5,16 +5,17 @@ class WhackARuby < Gosu::Window
 		super(800,600)
 		self.caption = 'Whack the Ruby!'
 		@image = Gosu::Image.new('ruby.png')
-		@x = 200
-		@y = 200
-		@width = 50
+		@x  = 200
+		@y  = 200
+		@width  = 50
 		@height = 43
-		@velocity_x = 5
-		@velocity_y = 5
+		@velocity_x  = 5
+		@velocity_y  = 5
 		@visible = 0
 		@hammer_image = Gosu::Image.new('hammer.png')
 		@hit = 0
 		@font = Gosu::Font.new(30)
+		@font_gameover = Gosu::Font.new(60)
 		@score = 0
 		@playing = true
 		@start_time = 0
@@ -25,11 +26,11 @@ class WhackARuby < Gosu::Window
 		if @playing
 			@x += @velocity_x
 			@y += @velocity_y
-			@velocity_x *= -1 if @x + @width  / 2 > 800 || @x - @width  / 2 < 0
-			@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
-			@visible -= 1
-			@visible = 30 if @visible < -10 && rand < 0.01
-			@time_left = (100 - ((Gosu.milliseconds - @start_time)/ 1000))
+			@velocity_x  *= -1 if @x  + @width  / 2 > 800 || @x  - @width  / 2 < 0
+			@velocity_y  *= -1 if @y  + @height / 2 > 600 || @y  - @height / 2 < 0
+			@visible  -= 1
+			@visible  = 30 if @visible  < -10 && rand < 0.01
+			@time_left = (30 - ((Gosu.milliseconds - @start_time)/ 1000))
 			@playing = false if @time_left < 0
 		end
 	end
@@ -37,7 +38,7 @@ class WhackARuby < Gosu::Window
 	def button_down(id)
 		if @playing
 			if (id == Gosu::MsLeft)
-				if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+				if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0 
         			@hit = 1
         			@score += 5
 				else
@@ -72,8 +73,8 @@ class WhackARuby < Gosu::Window
     	@font.draw(@score.to_s, 700, 20, 2)
     	@font.draw(@time_left.to_s, 20, 20, 2)
     	unless @playing
-    		@font.draw('Game Over', 300, 300, 3)
-    		@font.draw('Press the Space Bar to Play Again', 175, 350, 3)
+    		@font_gameover.draw('Game Over', 250, 300, 3)
+    		@font.draw('Press the Space Bar to Play Again', 175, 370, 3)
     		@visible = 20
     	end
 	end
