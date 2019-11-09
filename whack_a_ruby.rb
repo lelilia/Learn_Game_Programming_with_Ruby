a627c0197d31f7f3a2ecf3cc29da2ff43edf1206
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-11-08 19:28:22 -0500

    Jetzt mit Game Out

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -16,27 +16,33 @@ class WhackARuby < Gosu::Window
 		@hit = 0
 		@font = Gosu::Font.new(30)
 		@score = 0
+		@playing = true
 
 	end
 
 	def update
-		@x += @velocity_x
-		@y += @velocity_y
-		@velocity_x *= -1 if @x + @width  / 2 > 800 || @x - @width  / 2 < 0
-		@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
-		@visible -= 1
-		@visible = 30 if @visible < -10 && rand < 0.01
-		@time_left = (100 - (Gosu.milliseconds / 1000))
+		if @playing
+			@x += @velocity_x
+			@y += @velocity_y
+			@velocity_x *= -1 if @x + @width  / 2 > 800 || @x - @width  / 2 < 0
+			@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+			@visible -= 1
+			@visible = 30 if @visible < -10 && rand < 0.01
+			@time_left = (100 - (Gosu.milliseconds / 1000))
+			@playing = false if @time_left < 0
+		end
 	end
 
 	def button_down(id)
-		if (id == Gosu::MsLeft)
-			if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
-        		@hit = 1
-        		@score += 5
-			else
-				@hit = -1
-				@score -= 1
+		if @playing
+			if (id == Gosu::MsLeft)
+				if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+        			@hit = 1
+        			@score += 5
+				else
+					@hit = -1
+					@score -= 1
+				end
 			end
 		end
 	end
@@ -57,6 +63,10 @@ class WhackARuby < Gosu::Window
     	@hit = 0
     	@font.draw(@score.to_s, 700, 20, 2)
     	@font.draw(@time_left.to_s, 20, 20, 2)
+    	unless @playing
+    		@font.draw('Game Over', 300, 300, 3)
+    		@visible = 20
+    	end
 	end
 
 end
