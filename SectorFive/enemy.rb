72a0ea9c35f8785c1c15bde9012bf5cdaf2777ec
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-11-09 20:03:58 -0500

    new enemy class

---
+++ SectorFive/enemy.rb
@@ -0,0 +1,18 @@
+class Enemy
+	SPEED = 4
+
+	def initialize(window)
+		@radius = 20
+		@x = rand(window.width - 2 * @radius) + @radius
+		@y = 0
+		@image = Gosu::Image.new('images/enemy.png')
+	end
+
+	def move
+		@y += SPEED
+	end
+
+	def draw
+		@image.draw(@x - @radius, @y - @radius, 1)
+	end
+end
--- SectorFive/sector_five.rb
+++ SectorFive/sector_five.rb
@@ -1,17 +1,34 @@
 require 'gosu'
 require_relative 'player'
+require_relative 'enemy'
+
 
 class SectorFive < Gosu::Window
-	def initialize 
-		super(800,600)
+	WIDTH = 800
+	HEIGHT = 600
+
+	def initialize
+		super(WIDTH, HEIGHT)
 		self.caption = 'Sector Five'
 		@player = Player.new(self)
+		@enemy = Enemy.new(self)
+	end
+
+	def update
+		@player.turn_left  if button_down?(Gosu::KbLeft)
+		@player.turn_right if button_down?(Gosu::KbRight)
+		@player.accelerate if button_down?(Gosu::KbUp)
+		@player.move
+		@enemy.move
+	end
+
+	def draw
+		@player.draw
+		@enemy.draw
 	end
 end
 
-def draw
-	@player.draw
-end
+
 
 window = SectorFive.new
 window.show
