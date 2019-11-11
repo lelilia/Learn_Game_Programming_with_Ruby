565ab327f6935bfdc0aca6849c1d074b53b6fb36
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-11-11 13:08:02 -0500

    credit where credit is due

--- SectorFive/sector_five_scenes.rb
+++ SectorFive/sector_five_scenes.rb
@@ -3,11 +3,13 @@ require_relative 'player'
 require_relative 'enemy'
 require_relative 'bullet'
 require_relative 'explosion'
+require_relative 'credit'
 
 class SectorFive < Gosu::Window
 	WIDTH = 800
 	HEIGHT = 600
 	ENEMY_FREQUENCY = 0.05
+	MAX_ENEMIES = 100
 
 	def initialize
 		super(WIDTH, HEIGHT)
@@ -22,6 +24,8 @@ class SectorFive < Gosu::Window
 		@bullets = []
 		@explosions = []
 		@scene = :game
+		@enemies_appeared = 0
+		@enemies_destroyed = 0
 	end
 
 	def draw
@@ -68,6 +72,7 @@ class SectorFive < Gosu::Window
 		@player.move
 		if rand < ENEMY_FREQUENCY
 			@enemies.push Enemy.new(self)
+			@enemies_destroyed += 1
 		end
 		@enemies.each do |enemy|
 			enemy.move
@@ -82,6 +87,7 @@ class SectorFive < Gosu::Window
 					@enemies.delete enemy
 					@bullets.delete bullet
 					@explosions.push Explosion.new(self, enemy.x, enemy.y)
+					@enemies_destroyed += 1
 				end
 			end
 		end
@@ -96,6 +102,12 @@ class SectorFive < Gosu::Window
 		@bullets.dup.each do |bullet|
 			@bullets.delete bullet unless bullet.onscreen?
 		end
+		initialize_end(:count_reached) if @enemies_appeared > MAX_ENEMIES
+		@enemies.each do |enemy|
+			distance = Gosu.distance(enemy.x, enemy.y, @player.x, @player.y)
+			initialize_end(:hit_by_enemy) if distance < @player.radius + enemy.radius
+		end
+		initialize_end(:off_top) if @player.y < -@player.radius
 	end
 
 	def button_down(id)
