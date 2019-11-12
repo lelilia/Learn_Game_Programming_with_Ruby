e5a0bf958f916459fb8fc729e2bfa9abddc7517c
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-11-12 16:04:45 -0500

    tutorial fertig now with sound effects pew pew

--- SectorFive/sector_five_scenes.rb
+++ SectorFive/sector_five_scenes.rb
@@ -31,6 +31,8 @@ class SectorFive < Gosu::Window
 		@enemies_destroyed = 0
 		@game_music = Gosu::Song.new('sounds/Cephalopod.ogg')
 		@game_music.play(true)
+		@explosion_sound = Gosu::Sample.new('sounds/explosion.ogg')
+		@shooting_sound = Gosu::Sample.new('sounds/shoot.ogg')
 	end
 
 	def initialize_end(fate)
@@ -134,6 +136,7 @@ class SectorFive < Gosu::Window
 					@bullets.delete bullet 
 					@explosions.push Explosion.new(self, enemy.x, enemy.y)
 					@enemies_destroyed += 1
+					@explosion_sound.play
 				end
 			end
 		end
@@ -185,6 +188,7 @@ class SectorFive < Gosu::Window
 	def button_down_game(id)
 		if id == Gosu::KbSpace
 			@bullets.push Bullet.new(self, @player.x, @player.y, @player.angle)
+			@shooting_sound.play(0.3)
 		end 
 	end
 
