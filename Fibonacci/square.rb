17c08b814bfda00dc0504e876c5c80dff03f3f1b
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-11-22 18:11:34 -0500

    new game with Mai

---
+++ Fibonacci/square.rb
@@ -0,0 +1,92 @@
+require 'gosu'
+
+class Square
+	FONT_SIZE = 45
+	SQUARE_SIZE = 100
+	SQUARE_BORDER = 4
+	attr_reader :row, :col, :val
+
+	def initialize(window, col, row, val)
+		@@colors ||= {c1: Gosu::Color.argb(0xaac065eb),
+					  c2: Gosu::Color.argb(0xaaae2deb),
+					  c3: Gosu::Color.argb(0xaacb2deb),
+					  c5: Gosu::Color.argb(0xaaeb2deb),
+					  c8: Gosu::Color.argb(0xaac41ac4),
+					 c13: Gosu::Color.argb(0xaadb35b7),
+					 c21: Gosu::Color.argb(0xaad10fa7),
+					 c34: Gosu::Color.argb(0xaad64f9e),
+					 c55: Gosu::Color.argb(0xaae62294),
+				     c89: Gosu::Color.argb(0xaae62263),
+				    c144: Gosu::Color.argb(0xaaeb0753)}
+		@@window ||= window
+		@@font ||= Gosu::Font.new(FONT_SIZE)
+		@row = row
+		@col = col 
+		@val = val
+		@highlight = :default
+	end
+
+	def draw
+		if @val != 0
+			left   = 22 + @col * SQUARE_SIZE
+			top    = 22 + @row * SQUARE_SIZE
+			right  = left + SQUARE_SIZE - SQUARE_BORDER
+			bottom = top  + SQUARE_SIZE - SQUARE_BORDER
+			x_center = left + (SQUARE_SIZE - SQUARE_BORDER) / 2
+			x_text = x_center - @@font.text_width("#{@val}") / 2
+			y_text = top + (SQUARE_SIZE - SQUARE_BORDER - FONT_SIZE)/2
+
+			change = 0
+
+			if @val > 2048
+				@color = Gosu::Color.argb(0xaaeb07ff)
+			else
+				@color = ("c"+@val.to_s).to_sym
+			end
+			c = @@colors[@color]
+			
+			if @highlight == :new
+				@new_frame_count ||= 1
+				if @new_frame_count < 16
+					change = - SQUARE_SIZE * (16 - @new_frame_count) / 32
+					@new_frame_count += 1
+				else
+					@highlight = :default
+					@new_frame_count = nil 
+				end
+			elsif @highlight == :merge
+				@merge_frame_count ||= 1
+				if @merge_frame_count < 16
+					change = 4.0 / 16 * @merge_frame_count
+					@merge_frame_count += 1
+				else
+					@highlight = :default
+					@merge_frame_count = nil
+				end
+			end
+
+
+
+			
+
+			left -= change
+			right += change
+			top -= change
+			bottom += change
+
+			@@window.draw_quad(left, top, c, right, top, c, right, bottom, c, left, bottom, c, 1)
+			
+			@@font.draw_text("#{@val}", x_text, y_text, 2)
+
+		end
+	end
+
+	def clear
+		@val = 0
+	end
+
+	def set (val, highlight)
+		@val = val
+		@highlight = highlight
+	end
+end
