1b5bb6425280f01aa6a637c6db3786fefa459828
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-11-27 22:12:15 -0500

    playing with colors

--- Fibonacci/square.rb
+++ Fibonacci/square.rb
@@ -7,23 +7,30 @@ class Square
 	attr_reader :row, :col, :val
 
 	def initialize(window, col, row, val)
-		@@colors ||= {c1: Gosu::Color.argb(0xaac065eb),
-					  c2: Gosu::Color.argb(0xaaae2deb),
-					  c3: Gosu::Color.argb(0xaacb2deb),
-					  c5: Gosu::Color.argb(0xaaeb2deb),
-					  c8: Gosu::Color.argb(0xaac41ac4),
-					 c13: Gosu::Color.argb(0xaadb35b7),
-					 c21: Gosu::Color.argb(0xaad10fa7),
-					 c34: Gosu::Color.argb(0xaad64f9e),
-					 c55: Gosu::Color.argb(0xaae62294),
-				     c89: Gosu::Color.argb(0xaae62263),
-				    c144: Gosu::Color.argb(0xaaeb0753)}
+		#@@colors ||= {c1: Gosu::Color.argb(0xaac065eb),
+		#			  c2: Gosu::Color.argb(0xaaae2deb),
+		#			  c3: Gosu::Color.argb(0xaacb2deb),
+		#			  c5: Gosu::Color.argb(0xaaeb2deb),
+		#			  c8: Gosu::Color.argb(0xaac41ac4),
+		#			 c13: Gosu::Color.argb(0xaadb35b7),
+		#			 c21: Gosu::Color.argb(0xaad10fa7),
+		#			 c34: Gosu::Color.argb(0xaad64f9e),
+		#			 c55: Gosu::Color.argb(0xaae62294),
+		#		     c89: Gosu::Color.argb(0xaae62263),
+		#		    c144: Gosu::Color.argb(0xaaeb0753)}
 		@@window ||= window
 		@@font ||= Gosu::Font.new(FONT_SIZE)
 		@row = row
 		@col = col 
 		@val = val
 		@highlight = :default
+	end
+
+	def get_color(val)
+		r = (230 + 15 * val%10)%255
+		g = (170 - 10*val) % 255
+		b = (200 - 10 * val%10) %255
+		return Gosu::Color.argb(200 , r, g, b)
 	end
 
 	def draw
@@ -38,13 +45,13 @@ class Square
 
 			change = 0
 
-			if @val > 144
-				c = Gosu::Color.argb(0xaaeb07ff)
-			else
-				@color = ("c"+@val.to_s).to_sym
-				c = @@colors[@color]
-			end
-			
+			#if @val > 144
+			#	c = Gosu::Color.argb(0xaaeb07ff)
+			#else
+			#	@color = ("c"+@val.to_s).to_sym
+			#	c = @@colors[@color]
+			#end
+			c = get_color(@val)
 			
 			if @highlight == :new
 				@new_frame_count ||= 1
